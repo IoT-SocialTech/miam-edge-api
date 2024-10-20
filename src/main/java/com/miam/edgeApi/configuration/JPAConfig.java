49996fa22dc9cb1ced2f59c91086123c31c817b8
Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
2024-10-20 12:40:55 -0500

    feat: added JPA configuration and established connection with database

---
+++ src/main/java/com/miam/edgeApi/configuration/JPAConfig.java
@@ -0,0 +1,18 @@
+package com.miam.edgeApi.configuration;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+
+@Configuration
+public class JPAConfig {
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+}
