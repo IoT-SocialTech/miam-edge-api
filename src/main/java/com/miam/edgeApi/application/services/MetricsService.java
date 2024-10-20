4dd60290f6c0f0f8e8a470dc543b05c8ea40954a
Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
2024-10-20 14:08:57 -0500

    feat: added get average heart rate functionality
    feat: added functionality to return average patient heart rate
    Co-Authored-By: Jean Carlos Achamizo <traxefeder@gmail.com>
    Co-Authored-By: JesusRodriguez7 <89036050+JesusRodriguez7@users.noreply.github.com>
    Co-Authored-By: Luis Alberto Trujillo Lopez <104176051+NotLT42@users.noreply.github.com>
    Co-Authored-By: Rodrigo Raymundo  <130820365+RodrigoRG24@users.noreply.github.com>
    Co-Authored-By: Lucía A. <91093842+luciaaliagat@users.noreply.github.com>

--- src/main/java/com/miam/edgeApi/application/services/MetricsService.java
+++ src/main/java/com/miam/edgeApi/application/services/MetricsService.java
@@ -1,10 +1,13 @@
 package com.miam.edgeApi.application.services;
 
+import com.miam.edgeApi.application.dto.response.AverageHeartRateResponseDto;
 import com.miam.edgeApi.application.dto.response.HeartRateResponseDto;
 import com.miam.edgeApi.application.dto.response.TemperatureResponseDto;
 import com.miam.edgeApi.shared.model.dto.response.ApiResponse;
 
 public interface MetricsService {
+
+    ApiResponse<AverageHeartRateResponseDto> getAverageHeartRate();
 
     ApiResponse<HeartRateResponseDto> getHeartRate();
 
