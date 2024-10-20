71fbc15296bcda86101a869d6337d29a02bedff2
Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
2024-10-20 12:18:09 -0500

    Initial commit: added base files
    Initial commit: added base files for Miam edge Api project

---
+++ src/test/java/com/miam/edgeApi/EdgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miam.edgeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
