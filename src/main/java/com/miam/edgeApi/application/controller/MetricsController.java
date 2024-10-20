e1b80c717c8b00157ddba90b52ca7a4a0a58d90c
Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
2024-10-20 14:37:10 -0500

    feat: added get average temperature functionality
    feat: added functionality to return average patient temperature
    Co-Authored-By: Jean Carlos Achamizo <traxefeder@gmail.com>
    Co-Authored-By: JesusRodriguez7 <89036050+JesusRodriguez7@users.noreply.github.com>
    Co-Authored-By: Luis Alberto Trujillo Lopez <104176051+NotLT42@users.noreply.github.com>
    Co-Authored-By: Rodrigo Raymundo  <130820365+RodrigoRG24@users.noreply.github.com>
    Co-Authored-By: Lucía A. <91093842+luciaaliagat@users.noreply.github.com>

--- src/main/java/com/miam/edgeApi/application/controller/MetricsController.java
+++ src/main/java/com/miam/edgeApi/application/controller/MetricsController.java
@@ -1,6 +1,7 @@
 package com.miam.edgeApi.application.controller;
 
 import com.miam.edgeApi.application.dto.response.AverageHeartRateResponseDto;
+import com.miam.edgeApi.application.dto.response.AverageTemperatureResponseDto;
 import com.miam.edgeApi.application.dto.response.HeartRateResponseDto;
 import com.miam.edgeApi.application.dto.response.TemperatureResponseDto;
 import com.miam.edgeApi.application.services.MetricsService;
@@ -28,6 +29,13 @@ public class MetricsController {
     @GetMapping("/metrics/averageHeartRate")
     public ResponseEntity<ApiResponse<AverageHeartRateResponseDto>> getAverageHeartRate() {
         var res = metricsService.getAverageHeartRate();
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get average temperature")
+    @GetMapping("/metrics/averageTemperature")
+    public ResponseEntity<ApiResponse<AverageTemperatureResponseDto>> getAverageTemperature() {
+        var res = metricsService.getAverageTemperature();
         return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
